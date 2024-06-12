213fb65b829330f975e9141ca66eefb94b9ae82a
Nolan McCleary <ncm88@m4002-pc28.UBC_ECE>
2024-06-12 15:35:19 -0700

    add ram initialization shuffle

--- src/ksa.sv
+++ src/ksa.sv
@@ -12,9 +12,47 @@ module ksa
         input [6:0] HEX5
     );
 
-    logic clk;
+    logic clk, rst, write_enable;
+    
     assign clk = CLOCK_50;
-    assign rst = KEY[3];
+    assign rst = SW[0];
 
+    logic [7:0] address;
+    logic [7:0] ram_in;
+    logic [7:0] ram_out;
+
+    //read enable is by default high
+    ramcore S (
+        .address(address),
+        .clock(clk),
+        .data(ram_in),
+        .wren(write_enable),
+        .q(ram_out)
+    );
+
+
+    always @(posedge clk) begin
+        if(rst) begin
+            address <= 0;
+            ram_in <= 0;
+            write_enable <= 0;
+        end
+
+        else if(address < 255) begin
+            address <= address + 1;
+            ram_in <= ram_in + 1;
+            write_enable <= 1;
+        end
+
+        else begin
+            address <= address;
+            ram_in <= ram_in;
+            write_enable <= 0;
+        end
+    end
+
+
+
+                                      
 
 endmodule
