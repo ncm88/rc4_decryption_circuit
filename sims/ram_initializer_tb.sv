c3ad45d8f2a89e3826ad792bea2efeec6256e251
nolan <nolancmccleary@gmail.com>
2024-06-13 11:53:44 -0700

    working initializer tb

--- sims/ram_initializer_tb.sv
+++ sims/ram_initializer_tb.sv
@@ -1,6 +1,6 @@
 module ram_initializer_tb();
 
-    logic clk, start, write_enable, done, state_tap, clktap;
+    logic clk, start, write_enable, done;
     logic [7:0] ram_in;
     logic [7:0] address;
 
@@ -10,9 +10,7 @@ module ram_initializer_tb();
         .write_enable(write_enable),
         .ram_in(ram_in),
         .address(address),
-        .done(done),
-        .state_tap(state_tap),
-        .clktap(clktap)
+        .done(done)
     );
 
 
@@ -25,18 +23,22 @@ module ram_initializer_tb();
     initial begin
         clk = 0;
         start = 0;
-        #15
+        #150
         start = 1;
         #1700
         start = 0;
         #200
+        start = 1;
+        #500 
+        start = 0;
+        #270
         start = 1;
     end
 
 
 
     initial begin
-        #4000
+        #8000
         $finish;
     end
 
--- src/ram_initializer.sv
+++ src/ram_initializer.sv
@@ -11,123 +11,61 @@ module ram_initializer
         output logic [RAM_WIDTH - 1 : 0] ram_in,
         output logic [RAM_WIDTH - 1 : 0] address,
 
-        output logic done,
-        
-        output logic state_tap,
-        output logic clktap
+        output logic done
     );
 
-    localparam IDLE = 1'b0;
-    localparam SEED_SEQUENCE = 1'b1;
-
-    logic state;
-    logic next_state;
-    logic next_write_enable, next_done;
     logic [RAM_WIDTH - 1 : 0] next_ram_in;
     logic [RAM_WIDTH - 1 : 0] next_address;
 
+    logic enable;
+    logic next_done;
 
-    logic start_ack;
-    logic clear, next_clear;
-
-/*
-    //I love FPGAS
-    initial begin
-        state = IDLE;
-        clear = 0;
-        done = 0;
-        write_enable = 0;
-        ram_in = 0;
-        address = 0;
-        start_ack = 0;
-    end
-*/
-
-    assign clktap = clk;
 
     trap_edge trapper(
         .clk(clk),
-        .reset(clear),
+        .reset(done),
         .async_sig(start),
-        .trapped_edge(start_ack)
+        .trapped_edge(enable)
     );
 
-
-
-    //state change and clear logic
+    assign write_enable = enable;
+    
+    
     always_comb begin
-        case(state)
-            IDLE: begin
-                if(start_ack)begin
-                    next_state = SEED_SEQUENCE;
-                    next_clear = 1;
-                end
-            
-                else begin
-                    next_state = IDLE;
-                    next_clear = 0;
-                end
+        if(address < 255)begin
+            next_done = 0;
+            if(enable) begin
+                next_address = address + 1;
+                next_ram_in = ram_in + 1;
             end
-
-            SEED_SEQUENCE: begin
-                if(address < 255) begin
-                    next_state = SEED_SEQUENCE;
-                    next_clear = 0;
-                end
-                else begin
-                    next_state = IDLE;
-                    next_clear = 1;
-                end
+            else begin
+                next_address = 0;
+                next_ram_in = 0;
             end
-        endcase
+        end
+        else begin //thinks its at 256
+            if(enable) next_done = 1;
+            else next_done = 0;
+            next_address = 0;
+            next_ram_in = 0;
+        end
     end
 
 
 
     always_ff @(posedge clk) begin
-        clear <= next_clear;
-        state <= next_state;
-        address <= next_address;
-        ram_in <= next_ram_in;
-        write_enable <= next_write_enable;
-        done <= next_done;
-        state_tap <= state;
+        if(~enable) begin
+            address <= 0;
+            ram_in <= 0;
+            done <= 0;
+        end
+        else begin
+            address <= next_address;
+            ram_in <= next_ram_in;
+            done <= next_done;
+        end
     end
 
-
-    //output logic
-    always_comb begin 
-        case(state)
-            IDLE: begin
-                next_address = 0;
-                next_ram_in = 0;
-                if(start_ack) begin
-                    next_done = 0;
-                    next_write_enable = 1;
-                end
-                
-                else begin
-                    next_done = done;
-                    next_write_enable = 0;
-                end
-            end
-
-            SEED_SEQUENCE: begin
-                if(address < 255) begin
-                    next_address = address + 1;
-                    next_ram_in = ram_in + 1;
-                    next_write_enable = 1;
-                    next_done = 0;          //"Job's not finished" -Kobe
-                end
-                else begin
-                    next_address = 0;
-                    next_ram_in = 0;
-                    next_write_enable = 0;
-                    next_done = 1;
-                end
-            end
-        endcase
-    end
 
 
 
